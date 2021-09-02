b3003e83d2883b87600f64cbb4f2d8ed3e937bf2
Anouk Rimola <anouk138@gmail.com>
2021-09-01 19:03:44 -0600

    components

--- src/components/Inicio.js
+++ src/components/Inicio.js
@@ -1,32 +1,11 @@
 
-const Inicio = (props) => {
+const Inicio = () => {
 
-    const {carrusel} = props;
+    
 
     return ( 
         
-         <div id="carouselExampleCaptions" className="carousel slide" data-ride="carousel">
-            <ol className="carousel-indicators">
-                <li data-target="#carouselExampleCaptions" data-slide-to="0" className="active"></li>
-                <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
-                <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
-            </ol>
-            <div className="carousel-inner">
-                {carrusel.map((item=> <div className="carousel-item active">
-                <img src={"https://image.tmdb.org/t/p/original" + carrusel[0].backdrop_path} className=" w-100" alt="..." />
-                </div>))}
-                
-                
-            </div> 
-                <a className="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
-                    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-                    <span className="sr-only">Previous</span>
-                </a>
-                <a className="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
-                    <span className="carousel-control-next-icon" aria-hidden="true"></span>
-                    <span className="sr-only">Next</span>
-                </a>
-         </div>
+         <div></div>
         
      );
 }
---
+++ src/components/Tendencias.js
@@ -0,0 +1,10 @@
+
+const Tendencias = () => {
+    return ( 
+        <div>
+            
+        </div>
+     );
+}
+ 
+export default Tendencias;
