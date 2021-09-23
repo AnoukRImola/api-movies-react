268ebd0e3c6a9b3c79fb4266246a5cf7473d2fde
Anouk Rimola <anouk138@gmail.com>
2021-09-23 10:55:15 -0600

    update

--- src/components/Carrusel.js
+++ src/components/Carrusel.js
@@ -1,55 +1,25 @@
-import {Carousel} from 'react-bootstrap';
+import React, { useState, useEffect } from 'react';
+// import {Carousel} from 'react-bootstrap';
 
 const Carrusel = (props) => {
 
-     const{imagenCarrusel} = props;
-     console.log(props);
+    const [imagenCarrusel, setImagenCarrusel] = useState([]);
 
+    useEffect(()=>{
+        fetch('https://api.themoviedb.org/3/movie/popular?api_key=d6090b4ce2a41ad46da7767f56682434')
+        .then(response => response.json())
+        .then(data =>  {setImagenCarrusel(data.results)});
+    
+    },[])
+
+    console.log(imagenCarrusel);
     return ( 
-        <Carousel style={{height: "40rem"}} fade>
-        <Carousel.Item>
-             
-                <img
-                className="d-block w-100"
-                style={{height: "40rem"}}
-                src={"https://image.tmdb.org/t/p/original/7WJjFviFBffEJvkAms4uWwbcVUk.jpg"}
-                alt="First slide"
-            />
-           
-            
-            {/* <Carousel.Caption>
-                <h3 className="text-white">First slide label</h3>
-                <p>blabla.</p>
-            </Carousel.Caption> */}
-        </Carousel.Item>
-
-        <Carousel.Item>
-            <img
-                className="d-block w-100"
-                style={{height: "40rem"}}
-                src={"https://image.tmdb.org/t/p/original/dsdbViTNjLu4DbgkkYmuY4xDQ20.jpg"}
-                alt="Second slide"
-            />
-            {/* <Carousel.Caption>
-                <h3 className="text-white">Second slide label</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-            </Carousel.Caption> */}
-        </Carousel.Item>
-
-        <Carousel.Item>
-            <img
-                className="d-block w-100"
-                style={{height: "40rem"}}
-                src={"https://image.tmdb.org/t/p/original/kcvKEvTWqIGMjpVJdbDVRHdIt4C.jpg"}
-                alt="Third slide"
-            />
-            <Carousel.Caption>
-                <h3>vvv</h3>
-                <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-            </Carousel.Caption>
-        </Carousel.Item>
-      </Carousel>
-      
+        <div>
+            <h1>{imagenCarrusel.title} </h1>
+            <h1>{imagenCarrusel.title} </h1>
+            {/* <h1>{imagenCarrusel[0].title} </h1> */}
+        <p>jdjdjdjdjdjdjdjdjdsdjshsofhsdsdsjdjhhdhjsjghfjshdfhsdjkweidjsdji</p>
+      </div>
      );
 }
  
--- src/components/SectionCarrusel.js
+++ src/components/SectionCarrusel.js
@@ -1,22 +1,22 @@
-import React, { useState, useEffect } from 'react';
-import Carrusel from "./Carrusel";
+// import React, { useState, useEffect } from 'react';
+// import Carrusel from "./Carrusel";
 
-const SectionCarrusel = () => {
+// const SectionCarrusel = () => {
 
-    const [imagenCarrusel, setImagenCarrusel] = useState([]);
+//     const [imagenCarrusel, setImagenCarrusel] = useState([]);
 
-    useEffect(()=>{
-        fetch('https://api.themoviedb.org/3/movie/popular?api_key=d6090b4ce2a41ad46da7767f56682434')
-        .then(response => response.json())
-        .then(data =>  {setImagenCarrusel(data.results)});
+//     useEffect(()=>{
+//         fetch('https://api.themoviedb.org/3/movie/popular?api_key=d6090b4ce2a41ad46da7767f56682434')
+//         .then(response => response.json())
+//         .then(data =>  {setImagenCarrusel(data.results)});
     
-    },[])
+//     },[])
 
-    return ( 
-        <div>
-            <Carrusel imagenCarrusel={imagenCarrusel}/>
-        </div>
-     );
-}
+//     return ( 
+//         <div>
+//             <Carrusel imagenCarrusel={imagenCarrusel}/>
+//         </div>
+//      );
+// }
  
-export default SectionCarrusel;
+// export default SectionCarrusel;
