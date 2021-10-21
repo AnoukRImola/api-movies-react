2a90861156357964809f8ce0def4709a104a8afb
Anouk Rimola <anouk138@gmail.com>
2021-10-21 12:00:19 -0600

    update

--- src/components/Popular.js
+++ src/components/Popular.js
@@ -1,12 +1,13 @@
 
 const Popular = (props) => {
     return ( 
-        <div>
-            <div className="card border-0 scroll-item" style={{width: "10rem"}}>
-                <img src={"https://image.tmdb.org/t/p/original" + props.url} className="card-img-top border-0" alt="..." />
-                <div className="card-body px-0 py-2">
-                <h5 className="card-title text-white">{props.title}</h5>
-                </div>
+        <div className="section-scroll">
+            <div className="scroll-container">
+                 <div className="border-0">
+                    <div className="card border-0 scroll-item" style={{width: "10rem"}}>
+                        <img src={"https://image.tmdb.org/t/p/original" + props.url} className="card-img-top border-0" alt="..." />
+                    </div>
+                 </div>
             </div>
         </div>
      );
--- src/components/Proximas.js
+++ src/components/Proximas.js
@@ -1,13 +1,14 @@
 
 const Proximas = (props) => {
     return ( 
-        <div className="border-0">
-            <div className="card border-0 scroll-item" style={{width: "10rem"}}>
-                <img src={"https://image.tmdb.org/t/p/original" + props.url} className="card-img-top border-0" alt="..." />
-                {/* <div className="card-body px-0 py-2">
-                <h5 className="card-title text-white">{props.title}</h5>
-                </div> */}
-            </div>
+        <div className="section-scroll">
+            <div className="scroll-container">
+              <div className="border-0">
+                <div className="card border-0 scroll-item" style={{width: "10rem"}}>
+                    <img src={"https://image.tmdb.org/t/p/original" + props.url} className="card-img-top border-0" alt="..." />
+                </div>
+            </div>      
+           </div>
         </div>
      );
 }
