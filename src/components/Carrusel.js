7204f9290744db74db5f1adfc9c81153675ccd60
Anouk Rimola <anouk138@gmail.com>
2021-10-23 13:20:00 -0600

    update

--- src/components/Carrusel.js
+++ src/components/Carrusel.js
@@ -1,14 +1,14 @@
 import React, { useState, useEffect } from 'react';
 
 
-const Carrusel = (props) => {
+const Carrusel = () => {
 
     const [imagenCarrusel, setImagenCarrusel] = useState([]);
 
     useEffect(()=>{
         fetch('https://api.themoviedb.org/3/movie/popular?api_key=d6090b4ce2a41ad46da7767f56682434')
-        .then(response => response.json())
-        .then(data =>  setImagenCarrusel(data.results));
+        .then((response) => response.json())
+        .then((data) =>  setImagenCarrusel(data.results));
     
     },[])
 
@@ -18,15 +18,15 @@ const Carrusel = (props) => {
             <div id="carouselExampleCaptions" className="carousel slide mb-5" data-bs-ride="carousel">
                 <div className="carousel-inner">
                     
+                {imagenCarrusel.map((item,index)=> index ===0?
+                     <div className="carousel-item active">
                     
-                    {/* <div className="carousel-item active">
-                    
-                        <img src={'https://image.tmdb.org/t/p/w500'+imagenCarrusel.backdrop_path} className="d-block w-100" alt="..." />
-                    </div> */}
-                    {imagenCarrusel.length> 0 ? imagenCarrusel.map((item=>
+                        <img src={'https://image.tmdb.org/t/p/w500'+item.backdrop_path} className="d-block w-100" alt="..." />
+                    </div> :
+                   
                      <div className="carousel-item">
                         <img src={'https://image.tmdb.org/t/p/w500'+item.backdrop_path}  className="d-block w-100" alt="..." />
-                     </div> )):null}
+                     </div> )}
                 </div> 
             
                 <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
